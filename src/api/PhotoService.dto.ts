fa8a94de1818e241ae7ce30192056b586410317e
Vladislav Nozhkin <mega.nozhkin@pm.me>
2022-12-09 19:22:24 +0300

    changed PhotoService

--- src/api/PhotoService.dto.ts
+++ src/api/PhotoService.dto.ts
@@ -1,5 +1,5 @@
 export class PhotoServiceDto {
-  static async getAll(limit = 5) {
+  static async getAll(limit = 10) {
     return await fetch(
       `https://jsonplaceholder.typicode.com/photos?_limit=${limit}`
     );
